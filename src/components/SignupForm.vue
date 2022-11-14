a36b5384b167a97d361f7905057647321e37db43
wyne-ybanez <wyne_01@icloud.com>
2022-11-14 12:37:06 +0000

    Update: implement array data containment

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,32 +1,61 @@
 <template>
   <form>
+    <!-- Email -->
     <label>Email: </label>
-    <input type="email" required v-model="email"/>
+    <input type="email" required v-model="email" />
 
+    <!-- Password -->
     <label>Password: </label>
-    <input type="password" required v-model="password"/>
+    <input type="password" required v-model="password" />
 
+    <!-- Checkbox -->
     <label>Role: </label>
     <select v-model="role">
-        <option value="developer">Web Developer</option>
-        <option value="designer">Web Designer</option>
+      <option value="developer">Web Developer</option>
+      <option value="designer">Web Designer</option>
     </select>
 
+    <!-- Boolean -->
+    <div class="terms">
+      <input type="checkbox" v-model="terms" required />
+      <label>Accept terms and conditions</label>
+    </div>
+
+    <!-- Array: use this for multiple booleans -->
+    <div>
+      <input type="checkbox" value="shaun" v-model="names"/>
+      <label>Shaun</label>
+    </div>
+    <div>
+      <input type="checkbox" value="yoshi" v-model="names"/>
+      <label>Yoshi</label>
+    </div>
+    <div>
+      <input type="checkbox" value="mario" v-model="names"/>
+      <label>Mario</label>
+    </div>
+
   </form>
+
+  <h2 class="output-heading">Output:</h2>
   <p>Email: {{ email }}</p>
   <p>Password: {{ password }}</p>
   <p>Role: {{ role }}</p>
+  <p>Terms accepted: {{ terms }}</p>
+  <p>Names: {{ names }}</p>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            email: 'mario',
-            password: '',
-            role: '',
-        }
-    }
+  data() {
+    return {
+      email: "mario",
+      password: "",
+      role: "",
+      terms: false,
+      names: [],
+    };
+  },
 };
 </script>
 
@@ -39,19 +68,17 @@ form {
   padding: 40px;
   border-radius: 10px;
 }
-
 label {
   color: #aaa;
   display: inline-block;
-  /* top, right/left, bottom */
   margin: 25px 0 15px;
   font-size: 0.6em;
   text-transform: uppercase;
   letter-spacing: 1px;
   font-weight: bold;
 }
-
-input, select {
+input,
+select {
   display: block;
   padding: 10px 6px;
   width: 100%;
@@ -59,5 +86,16 @@ input, select {
   border: none;
   border-bottom: 1px solid #ddd;
   color: #555;
+}
+input[type="checkbox"] {
+  display: inline-block;
+  width: 16px;
+  margin: 0 10px 0 0;
+  position: relative;
+  top: 2px;
+}
+.output-heading {
+    position: relative;
+    right: 12.6rem;
 }
 </style>
